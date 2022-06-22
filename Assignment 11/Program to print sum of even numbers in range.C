f4d9cf85c26e3bab19fa49f8d96102d1d5824c2c
ShraddhaPatil10 <102403129+ShraddhaPatil10@users.noreply.github.com>
2022-06-22 07:37:03 +0530

    Add files via upload

---
+++ Assignment 11/Program to print sum of even numbers in range.C
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main ()
+{
+    int num=0,S_num=0,E_num=0,sum=0;
+    
+    printf("\n\"Note:Enter only positive numbers\"\n");
+    printf("\nEnter the range of number:\n");
+    
+    printf("\nEnter the starting number:");
+    scanf("%d",&S_num);
+    
+    printf("\nEnter the ending number:");
+    scanf("%d",&E_num);
+    
+    if(S_num>0 && E_num>0)
+    {
+        if(S_num<E_num)
+        {
+           for(num=S_num;num<=E_num;num++)
+          {
+              if(num%2==0)
+              {
+                 sum=sum+num;
+               }
+          }
+          printf("\nThe sum of the Even numbers in range are =>%d\n",sum);
+        }
+        else
+        {
+            printf("\nInvalid Range.\n");
+        }
+     }
+    else
+    {
+         printf("\nInvalid range.\n");
+    }
+    
+    getch();
+    return 0;
+}
