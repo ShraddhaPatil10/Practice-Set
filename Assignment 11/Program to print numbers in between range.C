04816767e8c5c5fc59c88b883b562e134530edc5
ShraddhaPatil10 <102403129+ShraddhaPatil10@users.noreply.github.com>
2022-06-22 07:17:27 +0530

    Add files via upload

---
+++ Assignment 11/Program to print numbers in between range.C
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main ()
+{
+    int num=0,S_num=0,E_num=0;
+    
+    printf("\nEnter the range of number:\n");
+    
+    printf("\nEnter the starting number:");
+    scanf("%d",&S_num);
+    
+    printf("\nEnter the ending number:");
+    scanf("%d",&E_num);
+    
+    if(S_num<E_num)
+    {
+        for(num=S_num;num<=E_num;num++)
+       {
+           printf(" %d ",num);
+       }
+    }
+    else
+    {
+         printf("\nInvalid range.\n");
+    }
+    
+    getch();
+    return 0;
+}
