dd395f62e3e88a4070bd73a3ac76f322f5bb0ecb
ShraddhaPatil10 <102403129+ShraddhaPatil10@users.noreply.github.com>
2022-06-23 12:09:49 +0530

    Add files via upload

---
+++ Assignment 14/Accept number from user and print 2 4 6 8,... pattern.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int num=0,i=0;
+
+    printf("\nEnter the number:\n\n");
+    scanf("%d",&num);
+
+    printf("\nPattern is=>\n");
+    for(i=1; i<=num; i++)
+    {
+        printf(" %d ",i*2);
+    }
+
+    getch();
+    return 0;
+}
