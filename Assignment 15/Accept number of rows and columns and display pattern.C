25530f4f20bc3bddadde1a065796264a970a5364
ShraddhaPatil10 <102403129+ShraddhaPatil10@users.noreply.github.com>
2022-06-23 12:14:06 +0530

    Add files via upload

---
+++ Assignment 15/Accept number of rows and columns and display pattern.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i=0,j=0,R=0,C=0;
+
+    printf("\nEnter number of rows:\n");
+    scanf("%d",&R);
+
+    printf("\nEnter number of columns:\n");
+    scanf("%d",&C);
+
+    for(i=1; i<=R; i++)
+    {
+        for(j=1; j<=C; j++)
+        {
+            printf(" * ");
+        }
+        printf("\n");
+    }
+
+    getch();
+    return 0;
+}
